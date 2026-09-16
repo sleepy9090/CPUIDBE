812eaf37714991c186323cd4612e69d20449aacf
sleepy9090 <sleepy3d@gmail.com>
2026-09-16 12:30:44 -0400

    Complete custom calls

--- CPUIDBE/cpuidbe.cpp
+++ CPUIDBE/cpuidbe.cpp
@@ -106,49 +106,6 @@ extern "C" __declspec(dllexport) const char* __cdecl GetCustom(unsigned int leaf
  * @param leaf The CPUID leaf value (e.g. 0x0, 0x1, 0x2...0x8000000A...).
  * @param subleaf The CPUID subleaf value (e.g. 0x0, 0x1, 0x2...).
  * @param registerIndex The index of the register to retrieve, EAX = 0x0, EBX = 0x1, ECX = 0x2, EDX = 0x3.
- * @param pos The starting position of the bits to extract (0-based index).
- * @param k The number of bits to extract starting from the position 'pos'.
- * @return A pointer to the binary string representing the register value in binary format.
- */
-extern "C" __declspec(dllexport) const char* __cdecl GetCustomRange(unsigned int leaf, unsigned int subleaf, unsigned int registerIndex, unsigned int pos, unsigned int k)
-{
-	// TODO: Implement the logic to extract the specified range of bits from the CPUID register value.
-    int cpuInfo[4];
-    __cpuidex(cpuInfo, leaf, subleaf);
-    char binaryStr[33];
-    IntToBinary32(cpuInfo[registerIndex], binaryStr);
-
-    char* result = (char*)malloc(33);
-    if (result) {
-        strcpy_s(result, 33, binaryStr);
-    }
-
-    return result;
-}
-
-/**
- * Gets the binary representation of a specific CPUID register value.
- * @param leaf The CPUID leaf value (e.g. 0x0, 0x1, 0x2...0x8000000A...).
- * @param subleaf The CPUID subleaf value (e.g. 0x0, 0x1, 0x2...).
- * @param registerIndex The index of the register to retrieve, EAX = 0x0, EBX = 0x1, ECX = 0x2, EDX = 0x3.
- * @param pos The starting position of the bits to extract (0-based index).
- * @return A pointer to the binary string representing the register value in binary format.
- */
-extern "C" __declspec(dllexport) int __cdecl GetCustomRangeFromPosition(unsigned int leaf, unsigned int subleaf, unsigned int registerIndex, unsigned int pos)
-{
-    int cpuInfo[4];
-    __cpuidex(cpuInfo, leaf, subleaf);
-
-    unsigned int result = ExtractBits(cpuInfo[registerIndex], pos, (32 - pos));
-
-    return result;
-}
-
-/**
- * Gets the binary representation of a specific CPUID register value.
- * @param leaf The CPUID leaf value (e.g. 0x0, 0x1, 0x2...0x8000000A...).
- * @param subleaf The CPUID subleaf value (e.g. 0x0, 0x1, 0x2...).
- * @param registerIndex The index of the register to retrieve, EAX = 0x0, EBX = 0x1, ECX = 0x2, EDX = 0x3.
  * @param k The number of bits to extract starting from the position 'pos'.
  * @return A pointer to the binary string representing the register value in binary format.
  */
